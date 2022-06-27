9350acf00ebda233597178cd88669f46967083ff
Raissaclark <98981306+Raissaclark@users.noreply.github.com>
2022-06-27 09:42:43 -0300

    Add files via upload

--- PlayerArrow.js
+++ PlayerArrow.js
@@ -13,16 +13,18 @@ class PlayerArrow {
     World.add(world, this.body);
   }
 
-  remove(index) {
-    this.isRemoved = true;
+   remove(index) {
+    this.isRemoved = false;
     Matter.World.remove(world, this.body);
     delete playerArrows[index];
-  }
+  }  
+
+
 
   shoot(archerAngle) {
     this.velocity = p5.Vector.fromAngle(archerAngle + PI / 2);
     this.velocity.mult(55);
-
+Matter.World.remove(this.body,world)
     Matter.Body.setVelocity(this.body, {
       x: this.velocity.x,
       y: this.velocity.y
@@ -50,15 +52,5 @@ class PlayerArrow {
     imageMode(CENTER);
     image(this.image, 0, 0, this.width, this.height);
     pop();
-
-    if (this.body.velocity.x > 0 && this.body.position.x > 400) {
-      var position = [this.body.position.x, this.body.position.y];
-      this.trajectory.push(position);
-    }
-
-    for (var i = 0; i < this.trajectory.length; i++) {
-      fill("white");
-      ellipse(this.trajectory[i][0], this.trajectory[i][1], 5, 5);
-    }
   }
 }
